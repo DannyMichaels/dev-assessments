cfc3ce0805fbd1fdecf9e7e55614182808a0d142
Daniel <itzdanielmichael@gmail.com>
2020-12-27 10:41:12 -0500

    separated way 2

--- test_2/index.js
+++ test_2/index.js
@@ -11,29 +11,19 @@
  *
  * @returns  {Object[]}
  */
-// we do this so we can get the results from test 1
 // our task will be to filter the output from your first test so that only characters with the `last_name` of "Simpson" will be returned
 
-// way 1 require
-const test1 = require("../test_1/index");
-
-// way 2 require
-// let allCharacters = require("./test_data.json");
+let allCharacters = require("./test_data.json");
 
 module.exports = function test2() {
-  // 1 liner way of doing it
-  let results = test1().filter((simpson) => simpson.last_name === "Simpson");
+  let results = allCharacters.filter(
+    (character) => character.last_name === "Simpson"
+  );
 
-  // 2nd way of doing it
-  // let results2 = allCharacters.filter(
-  //   (character) => character.last_name === "Simpson"
-  // );
-
-  // results2 = results2.map((character) => ({
-  //   ...character,
-  //   example: `${character.first_name} ${character.last_name} says ${character.catchphrase}`,
-  // }));
+  results = results.map((character) => ({
+    ...character,
+    example: `${character.first_name} ${character.last_name} says ${character.catchphrase}`,
+  }));
 
   return results;
-  // return results2;
 };
